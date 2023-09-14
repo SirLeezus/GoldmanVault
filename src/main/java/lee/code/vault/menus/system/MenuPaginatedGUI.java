5b2c3dbb8aa4dfc74ae5c26ab8c6b8bd27b51a42
Lee <leecook808@gmail.com>
2023-09-13 20:18:53 -0700

    updates

--- src/main/java/lee/code/vault/menus/system/MenuPaginatedGUI.java
+++ src/main/java/lee/code/vault/menus/system/MenuPaginatedGUI.java
@@ -10,19 +10,16 @@ import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public abstract class MenuPaginatedGUI implements InventoryHandler {
-  private final List<Integer> border = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 17, 18, 26, 27, 35, 36, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53);
-  protected final List<Integer> paginatedSlots = Arrays.asList(10, 11, 12, 13, 14, 15, 16, 19, 20, 21, 22, 23, 24, 25, 28, 29, 30, 31, 32, 33, 34, 37, 38, 39, 40, 41, 42, 43);
+  private final List<Integer> border = Arrays.asList(45, 46, 47, 48, 49, 50, 51, 52, 53);
+  protected final List<Integer> paginatedSlots = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44);
   protected int page = 0;
   protected int index = 0;
-  protected final int maxItemsPerPage = 28;
+  protected final int maxItemsPerPage = 45;
   private Inventory inventory;
-  public final ItemStack fillerGlass = MenuItem.FILLER_GLASS.createItem();
+  private final ItemStack fillerGlass = MenuItem.FILLER_GLASS.createItem();
   private final DelayManager delayManager = new DelayManager();
   private final Map<Integer, MenuButton> buttonMap = new HashMap<>();
   @Getter private final MenuSoundManager menuSoundManager = new MenuSoundManager();
