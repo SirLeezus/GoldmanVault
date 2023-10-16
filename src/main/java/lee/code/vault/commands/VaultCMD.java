00d9695f31235d6f2b6bbcfd6a8dfb5f67f9c1d9
Lee <leecook808@gmail.com>
2023-10-16 10:22:37 -0700

    HelpCMD update

--- src/main/java/lee/code/vault/commands/VaultCMD.java
+++ src/main/java/lee/code/vault/commands/VaultCMD.java
@@ -4,11 +4,15 @@ import lee.code.vault.Vault;
 import lee.code.vault.lang.Lang;
 import lee.code.vault.menus.menu.VaultMenu;
 import lee.code.vault.menus.system.MenuManager;
+import net.kyori.adventure.text.Component;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class VaultCMD implements CommandExecutor {
   private final Vault vault;
@@ -21,6 +25,20 @@ public class VaultCMD implements CommandExecutor {
   public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
     if (!(sender instanceof Player player)) {
       sender.sendMessage(Lang.PREFIX.getComponent(null).append(Lang.ERROR_NOT_CONSOLE_COMMAND.getComponent(null)));
+      return true;
+    }
+    if (args.length > 0 && args[0].equalsIgnoreCase("help")) {
+      final List<Component> lines = new ArrayList<>();
+      lines.add(Lang.COMMAND_HELP_TITLE.getComponent(null));
+      lines.add(Component.text(""));
+      lines.add(Lang.COMMAND_HELP_LINE_1.getComponent(null));
+      lines.add(Lang.COMMAND_HELP_LINE_2.getComponent(null));
+      lines.add(Lang.COMMAND_HELP_LINE_3.getComponent(null));
+      lines.add(Lang.COMMAND_HELP_LINE_4.getComponent(null));
+      lines.add(Lang.COMMAND_HELP_LINE_5.getComponent(null));
+      lines.add(Component.text(""));
+      lines.add(Lang.COMMAND_HELP_FOOTER.getComponent(null));
+      for (Component line : lines) sender.sendMessage(line);
       return true;
     }
     final MenuManager menuManager = vault.getMenuManager();
